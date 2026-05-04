385cba7de34fb6838194956c43214422e3e2c771
bobbyporterprojects <bobby.porter11@gmail.com>
2026-05-03 20:50:32 -0400

    Add Baned Bandit and pip-audit set security scans

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -10,7 +11,10 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = "dev-secret-key-change-later"
+    app.config["SECRET_KEY"] = os.environ.get(
+	"SECRET_KEY",
+	"dev-secret-key-change-later"
+	)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///security_study_tracker.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
